67ad75c2952b94218cc53b0741f51e13616294c7
mickexamp <mimymy13@yandex.ua>
2018-07-05 00:22:36 +0300

    Exercise3
    Calculation of the distance to the place of lightning strike.

---
+++ Exercise3.py
@@ -0,0 +1,7 @@
+speed = 1100
+scn = input("Enter the number of seconds after the flash: ")
+c = int(str(scn))
+b = speed * c
+print("The distance is: ", b, "ft")
+print("\n \n Have a nice day! \n 07.05.18.MikeMoll")
+
